f2394d61910419b2799d7ccde1525814b9a6ae1d
KhoaPC <luongvankhoapc@gmail.com>
2022-05-05 14:45:18 +0700

    22May05 FIX layout code js

--- pages/bai-xep/script.js
+++ pages/bai-xep/script.js
@@ -1,1 +1,13 @@
 console.log('Hello from /pages/bai-xep/script.js');
+
+
+
+
+function myFunction() {
+    var x = document.getElementById("menu-nav");
+    if (x.className === "header-center") {
+        x.className += "responsive";
+    } else {
+        x.className = "header-center";
+    }
+}
--- pages/ganh-da-dia/script.js
+++ pages/ganh-da-dia/script.js
@@ -1,1 +1,12 @@
 console.log('Hello from /pages/ganh-da-dia/script.js');
+
+
+
+function myFunction() {
+    var x = document.getElementById("menu-nav");
+    if (x.className === "header-center") {
+        x.className += "responsive";
+    } else {
+        x.className = "header-center";
+    }
+}
