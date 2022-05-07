6fa30d49badb4c2bb5cec3faa678d938722809d7
KhoaPC <luongvankhoapc@gmail.com>
2022-05-07 09:50:17 +0700

    22May07 FIX menu-nav hidden when submenu indicator is clicked

--- pages/ganh-da-dia/script.js
+++ pages/ganh-da-dia/script.js
@@ -3,19 +3,28 @@ console.log('Hello from /pages/ganh-da-dia/script.js');
 
 
 // Hide responsive menu on clicking anywhere except for header
-document.onclick = function () {
-    const x = document.getElementById("menu-nav");
+document.onclick = function (event) {
+  // (1#) Detect the clicked element            
+  // (2#) Check if it has class 'header-center__submenu-indicator'
+  // (3#) If yes, ignore the hiding action       
+  const clickedElement = event.target; // (#1)
+  const check = clickedElement.classList.contains('header-center__submenu-indicator'); // (#2)
+  if (check === true)
+    return; // ignore the remaining codes
+
+  // Menu nav (center div)
+  const x = document.getElementById("menu-nav");
+  x.className = "header-center";
+} // document.onclick
+
+function myFunction() {
+  var x = document.getElementById("menu-nav");
+  if (x.className === "header-center") {
+    x.className += "responsive";
+  } else {
     x.className = "header-center";
-  } // document.onclick
-
-  function myFunction() {
-    var x = document.getElementById("menu-nav");
-    if (x.className === "header-center") {
-      x.className += "responsive";
-    } else {
-      x.className = "header-center";
-    }
-
-    // Prevent event fired to the parent (document)
-    event.stopPropagation();
   }
+
+  // Prevent event fired to the parent (document)
+  event.stopPropagation();
+}
